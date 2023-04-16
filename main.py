8721c035dc550a4905e6be2f5e5b0c3e23143ce2
the-genius01 <77913516+the-genius01@users.noreply.github.com>
2023-04-16 14:06:22 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,26 @@
+from typing import List
+from fastapi import FastAPI
+from pydantic import BaseModel
+import openai
+
+openai.api_key = "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"
+
+app = FastAPI()
+
+class SearchRequest(BaseModel):
+    query: str
+
+class SearchResponse(BaseModel):
+    response: str
+
+@app.post("/search", response_model=SearchResponse)
+def search(request: SearchRequest) -> SearchResponse:
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=request.query,
+        max_tokens=500,
+        n=1,
+        stop=None,
+        temperature=0.5
+    )
+    return SearchResponse(response=response.choices[0].text)
